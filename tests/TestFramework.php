1835f8b2cd131423450ed65e8c5af74a60e4f7d2
velliz <diditvelliz@outlook.com>
2016-10-25 20:16:45 +0700

    test case

--- tests/TestFramework.php
+++ tests/TestFramework.php
@@ -1,12 +1,19 @@
 <?php
 
-class TestFramework extends \PHPUnit_Framework_TestCase
+namespace tests;
+
+use phpunit\framework\TestCase;
+use pukoframework\Request;
+
+class TestFramework extends TestCase
 {
 
-    public function TestRequest()
+    public function testRequest()
     {
-        $framework = new \pukoframework\Framework();
-        assertInstanceOf(get_class($framework), $framework);
+        include '../vendor/autoload.php';
+        $framework = new Request();
+        $this->assertEquals("main", "main");
+        $this->assertEquals("main", "main");
     }
 
 }
