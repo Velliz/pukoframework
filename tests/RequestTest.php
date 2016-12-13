e4473030dfcfb7b8733d0b28449b1e6c64c6e9fd
velliz <diditvelliz@outlook.com>
2016-12-14 00:15:50 +0700

    covealls test case update

---
+++ tests/RequestTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace tests;
+
+use PHPUnit_Framework_TestCase;
+use pukoframework\pte\RenderEngine;
+use pukoframework\Request;
+
+class RequestTest extends PHPUnit_Framework_TestCase
+{
+
+    public function setUp()
+    {
+    }
+
+    public function tearDown()
+    {
+    }
+
+    public function testRender()
+    {
+        $pte = new RenderEngine('string');
+        $this->assertTrue($pte->clearOutput);
+    }
+
+    public function testRequest()
+    {
+        $_GET['request'] = 'puko/tests/1';
+        $_GET['lang'] = 'id';
+        $req = new Request();
+
+        $this->assertEquals('puko', $req->className);
+        $this->assertEquals('tests', $req->fnName);
+        $this->assertEquals('id', $req->lang);
+    }
+
+    public function testPost()
+    {
+        $user = Request::Post('user', 0);
+        $this->assertEquals(0, $user);
+
+        $_POST['framework'] = 'puko';
+        $fw = Request::Post('framework', 0);
+        $this->assertEquals('framework', $fw);
+    }
+
+    public function testGet()
+    {
+        $_GET['user'] = 'puko';
+        $user = Request::Get('user', 0);
+        $this->assertEquals('puko', $user);
+
+        $fw = Request::Get('framework', 0);
+        $this->assertEquals(0, $fw);
+    }
+
+    public function testIsPost()
+    {
+        $isPost = Request::IsPost();
+        $this->assertFalse($isPost);
+    }
+
+}
