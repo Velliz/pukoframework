e40ec140e44bf91aad3197b03ed9a0b2fca8c2b3
velliz <diditvelliz@outlook.com>
2016-12-20 20:49:13 +0700

    pragma and cache

--- src/pte/RenderEngine.php
+++ src/pte/RenderEngine.php
@@ -109,7 +109,11 @@ namespace pukoframework\pte {
         public function Auth($val)
         {
             if ($val === 'true') {
-                header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+                header('Expires: Mon, 1 Jul 1998 01:00:00 GMT');
+                header('Cache-Control: no-store, no-cache, must-revalidate');
+                header('Cache-Control: post-check=0, pre-check=0', false);
+                header('Pragma: no-cache');
+                header( "Last-Modified: " . gmdate( "D, j M Y H:i:s" ) . " GMT" );
                 if (!Session::IsSession()) {
                     throw new \Exception('Authentication Required');
                 }
