e3992a15fe88a47a9e564b27a6d7392f9476f49c
Velliz <diditvelliz@gmail.com>
2017-02-28 15:43:02 +0700

    add new type of exception

---
+++ src/peh/ThrowService.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * pukoframework.
+ *
+ * MVC PHP Framework for quick and fast PHP Application Development.
+ *
+ * This content is released under the Apache License Version 2.0, January 2004
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Copyright (c) 2016, Didit Velliz
+ *
+ * @author    Didit Velliz
+ *
+ * @link    https://github.com/velliz/pukoframework
+ * @since    Version 1.0.2
+ */
+
+namespace pukoframework\peh;
+
+use Exception;
+use pukoframework\Lifecycle;
+use pukoframework\pte\RenderEngine;
+
+class ThrowService extends Exception
+{
+
+    public $IsFatalError;
+
+    /**
+     * PukoException constructor.
+     *
+     * @param string $message
+     */
+    public function __construct($message = '')
+    {
+        parent::__construct($message, 10222, null);
+    }
+
+    /**
+     * @param Exception $error
+     *
+     * @return mixed
+     */
+    public function ExceptionHandler($error)
+    {
+        $emg['Message'] = $error->getMessage();
+        $emg['File'] = $error->getFile();
+        $emg['LineNumber'] = $error->getLine();
+
+        header('Author: Puko Framework');
+        header('Content-Type: application/json');
+
+        $data = array(
+            'time' => microtime(true) - Lifecycle::$start,
+            'status' => 'failed',
+            'exception' => $emg
+        );
+
+        echo json_encode($data);
+    }
+
+    /**
+     * @param $error
+     * @param $message
+     * @param $file
+     * @param $line
+     *
+     * @return array
+     */
+    public function ErrorHandler($error, $message, $file, $line)
+    {
+        $emg['Error'] = $error;
+        $emg['Message'] = $message;
+        $emg['File'] = $file;
+        $emg['LineNumber'] = $line;
+
+        header('Author: Puko Framework');
+        header('Content-Type: application/json');
+
+        $data = array(
+            'time' => microtime(true) - Lifecycle::$start,
+            'status' => 'failed',
+            'exception' => $emg
+        );
+
+        echo json_encode($data);
+    }
+}
---
+++ src/peh/ThrowView.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * pukoframework.
+ *
+ * MVC PHP Framework for quick and fast PHP Application Development.
+ *
+ * This content is released under the Apache License Version 2.0, January 2004
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Copyright (c) 2016, Didit Velliz
+ *
+ * @author    Didit Velliz
+ *
+ * @link    https://github.com/velliz/pukoframework
+ * @since    Version 1.0.2
+ */
+
+namespace pukoframework\peh;
+
+use Exception;
+use pukoframework\pte\RenderEngine;
+
+class ThrowView extends Exception
+{
+    /**
+     * @var RenderEngine
+     */
+    private $render;
+
+    public $IsFatalError;
+    public $systemHtml = ROOT.'/assets/system/';
+
+    /**
+     * PukoException constructor.
+     *
+     * @param string $message
+     */
+    public function __construct($message = '')
+    {
+        parent::__construct($message, 10122, null);
+        $this->render = new RenderEngine();
+        $this->render->useMasterLayout = false;
+    }
+
+    /**
+     * @param Exception $error
+     *
+     * @return mixed
+     */
+    public function ExceptionHandler($error)
+    {
+        $emg['Message'] = $error->getMessage();
+        $emg['File'] = $error->getFile();
+        $emg['LineNumber'] = $error->getLine();
+        echo $this->render->PTEParser($this->systemHtml.'/exception.html', $emg);
+    }
+
+    /**
+     * @param $error
+     * @param $message
+     * @param $file
+     * @param $line
+     */
+    public function ErrorHandler($error, $message, $file, $line)
+    {
+        $emg['Error'] = $error;
+        $emg['Message'] = $message;
+        $emg['File'] = $file;
+        $emg['LineNumber'] = $line;
+        echo $this->render->PTEParser($this->systemHtml.'/error.html', $emg);
+    }
+}
