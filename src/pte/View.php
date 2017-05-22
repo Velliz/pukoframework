ad1f409f7aaf9dc22aadccf0f0925eff5d1edaf8
Velliz <diditvelliz@gmail.com>
2017-05-22 09:32:45 +0700

    recode PDC view exception handler

--- src/pte/View.php
+++ src/pte/View.php
@@ -3,14 +3,16 @@
 namespace pukoframework\pte;
 
 use pukoframework\auth\Auth;
+use pukoframework\peh\ThrowView;
+use pukoframework\Response;
 
 class View extends Controller implements Auth
 {
     public function __construct()
     {
-//        $exceptionHandler = new ThrowView();
-//        set_exception_handler(array($exceptionHandler, 'ExceptionHandler'));
-//        set_error_handler(array($exceptionHandler, 'ErrorHandler'));
+        $exceptionHandler = new ThrowView('View Error', new Response());
+        set_exception_handler(array($exceptionHandler, 'ExceptionHandler'));
+        set_error_handler(array($exceptionHandler, 'ErrorHandler'));
     }
 
     public function Login($username, $password)
